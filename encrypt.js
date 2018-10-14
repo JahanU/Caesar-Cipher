6c5ebb1bf87ce7a9c51c2999cdd1868b7f7582ba
Jahan <34219176+JahanUoL@users.noreply.github.com>
2018-10-14 17:14:29 +0100

    Create encrypt.js

---
+++ encrypt.js
@@ -0,0 +1,49 @@
+
+var btnEncrypt = document.getElementById("btnEncrypt");
+var encryptedMessasgeResult = document.getElementById("encryptedMessasgeResult");
+
+btnEncrypt.addEventListener("click", function() {
+	encryptedMessasgeResult.innerHTML = "";
+	var str = document.getElementById("encryptMessage").value;
+	var amount = document.getElementById("shift").value;
+	caesarShift(str, Number(amount));
+})
+
+var caesarShift = function(str, amount) {
+
+	// Wrap the amount
+	if (amount < 0)
+		return caesarShift(str, amount + 26);
+
+	// Make an output variable
+	var output = '';
+
+	// Go through each character
+	for (var i = 0; i < str.length; i ++) {
+
+		// Get the character we'll be appending
+		var c = str[i];
+
+		// If it's a letter...
+		if (c.match(/[a-z]/i)) {
+
+			// Get its code
+			var code = str.charCodeAt(i);
+
+			// Uppercase letters
+			if ((code >= 65) && (code <= 90)) 
+				c = String.fromCharCode(((code - 65 + amount) % 26) + 65);
+        
+			// Lowercase letters
+			else if ((code >= 97) && (code <= 122))
+				c = String.fromCharCode(((code - 97 + amount) % 26) + 97);
+
+		}
+		// Append
+		output += c;
+	}
+	encryptedMessasgeResult.innerHTML = output;
+
+	return output;
+
+};
